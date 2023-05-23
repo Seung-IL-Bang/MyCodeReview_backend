72104ec589a857f2998c0c364cec507b6a4636f3
Seungil <seungilbang@khu.ac.kr>
2023-05-23 20:18:59 +0900

    [ADD] DefaultOAuth2UserService: 소셜 인증 처리

---
+++ src/main/java/com/web/app/security/auth/CustomOAuth2UserService.java
@@ -0,0 +1,22 @@
+package com.web.app.security.auth;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Log4j2
+@Service
+@RequiredArgsConstructor
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+
+        return null;
+    }
+}
