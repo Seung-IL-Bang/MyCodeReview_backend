dbf3f4d14333251fe3867f424af49c4c82620766
Seungil <seungilbang@khu.ac.kr>
2023-05-23 20:17:49 +0900

    [ADD] AuthenticationSuccessHandler: 소셜 로그인 성공 후 핸들러

---
+++ src/main/java/com/web/app/security/handler/CustomSocialLoginSuccessHandler.java
@@ -0,0 +1,24 @@
+package com.web.app.security.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
+
+@Log4j2
+@RequiredArgsConstructor
+public class CustomSocialLoginSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        log.info("========================== Social Login Success Handler ==========================");
+    }
+}
