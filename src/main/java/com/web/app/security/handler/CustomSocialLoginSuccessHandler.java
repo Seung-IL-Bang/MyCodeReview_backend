146d2b956071907b3cbefe16c7a712b50d52240e
Seungil <seungilbang@khu.ac.kr>
2023-05-26 10:16:26 +0900

    [UPD] CustomSocialLoginSuccessHandler: 상속 변경 & 쿠키 추가 & 리다이렉팅 URI 변경

--- src/main/java/com/web/app/security/handler/CustomSocialLoginSuccessHandler.java
+++ src/main/java/com/web/app/security/handler/CustomSocialLoginSuccessHandler.java
@@ -1,26 +1,25 @@
 package com.web.app.security.handler;
 
-import com.google.gson.Gson;
 import com.web.app.security.dto.MemberSecurityDTO;
 import com.web.app.util.JWTUtil;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
 import java.util.Map;
 
 @Log4j2
 @RequiredArgsConstructor
-public class CustomSocialLoginSuccessHandler implements AuthenticationSuccessHandler {
+public class CustomSocialLoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
-    private final PasswordEncoder passwordEncoder;
     private final JWTUtil jwtUtil;
 
     @Override
@@ -38,13 +37,30 @@ public class CustomSocialLoginSuccessHandler implements AuthenticationSuccessHan
         String accessToken = jwtUtil.generateToken(claims, 1);
         String refreshToken = jwtUtil.generateToken(claims, 30);
 
-        Gson gson = new Gson();
+        String uri = UriComponentsBuilder
+                .newInstance()
+                .scheme("http")
+                .host("localhost")
+                .port(3000)
+                .path("/")
+                .build()
+                .toUri()
+                .toString();
 
-        Map<String, Object> tokenMap = Map.of("accessToken", accessToken, "refreshToken", refreshToken);
 
-        String json = gson.toJson(tokenMap);
+        Cookie[] cookies = {
+                new Cookie("accessToken", accessToken),
+                new Cookie("refreshToken", refreshToken)
+        };
 
-        response.getWriter().println(json);
-//        response.sendRedirect("/");
+        for (Cookie cookie : cookies) {
+            cookie.setMaxAge(30 * 60);
+            cookie.setDomain("localhost");
+            cookie.setPath("/");
+            cookie.setSecure(true);
+            response.addCookie(cookie);
+        }
+
+        getRedirectStrategy().sendRedirect(request, response, uri);
     }
 }
