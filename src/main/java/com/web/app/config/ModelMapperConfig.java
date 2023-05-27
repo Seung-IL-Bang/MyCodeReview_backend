2bd108577b95e3929b9a8523f16dadfed4943688
Seungil <seungilbang@khu.ac.kr>
2023-05-27 22:17:04 +0900

    [ADD] ModelMapperConfig

---
+++ src/main/java/com/web/app/config/ModelMapperConfig.java
@@ -0,0 +1,23 @@
+package com.web.app.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Log4j2
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper getMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setFieldMatchingEnabled(true)
+                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+                .setMatchingStrategy(MatchingStrategies.LOOSE);
+
+        return modelMapper;
+    }
+}
