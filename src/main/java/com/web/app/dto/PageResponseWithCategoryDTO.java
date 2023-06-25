4712d012b419fbfac4b74236d8665ea9c25a4fef
Bang <seungilbang@khu.ac.kr>
2023-06-25 17:01:37 +0900

    [ADD] PageResponseWithCategoryDTO: 사용자별 태그 목록 포함한 페이지 응답 DTO

---
+++ src/main/java/com/web/app/dto/PageResponseWithCategoryDTO.java
@@ -0,0 +1,55 @@
+package com.web.app.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@ToString
+public class PageResponseWithCategoryDTO<E> {
+
+    private int page;
+    private int size;
+    private int total;
+
+    // 시작 페이지 번호
+    private int start;
+    // 끝 페이지 번호
+    private int end;
+
+    // 이전 페이지의 존재 여부
+    private boolean prev;
+    // 다음 페이지의 존재 여부
+    private boolean next;
+
+    private List<E> dtoList;
+
+    private Map<String, Integer> dtoTags;
+
+    @Builder
+    public PageResponseWithCategoryDTO(PageRequestDTO pageRequestDTO, List<E> dtoList, int total, Map<String, Integer> dtoTags) {
+
+        this.page = pageRequestDTO.getPage();
+        this.size = pageRequestDTO.getSize();
+        this.dtoList = dtoList;
+        this.total = total;
+        this.dtoTags = dtoTags;
+
+        this.end = (int)(Math.ceil(this.page / 10.0)) * 10;
+
+        this.start = end - 9;
+
+        int last = (int)(Math.ceil(total/(double) size));
+
+        this.end = Math.min(end, last);
+
+        this.prev = this.start > 1;
+
+        this.next = total > this.end * this.size;
+
+    }
+
+}
