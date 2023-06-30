e500e4dbfbfa4099426ac951c58d290d0447dc48
Bang <seungilbang@khu.ac.kr>
2023-06-30 18:46:18 +0900

    [UPD] BoardSearch: 필터된 데이터 총 합 Count 메소드 추가

--- src/main/java/com/web/app/repository/search/BoardSearch.java
+++ src/main/java/com/web/app/repository/search/BoardSearch.java
@@ -7,4 +7,6 @@ import org.springframework.data.domain.Pageable;
 public interface BoardSearch {
 
     Page<Board> searchAll(String[] types, String email, String keyword, String[] difficulties, String tag, Pageable pageable);
+
+    long filteredAll(String[] types, String email, String keyword, String[] difficulties, String tag);
 }
--- src/main/java/com/web/app/repository/search/BoardSearchImpl.java
+++ src/main/java/com/web/app/repository/search/BoardSearchImpl.java
@@ -59,4 +59,41 @@ public class BoardSearchImpl extends QuerydslRepositorySupport implements BoardS
 
         return new PageImpl<>(list, pageable, count);
     }
+
+    @Override
+    public long filteredAll(String[] types, String email, String keyword, String[] difficulties, String tag) {
+
+        QBoard board = QBoard.board;
+
+        JPQLQuery<Board> query = from(board);
+
+        if ((types != null && types.length > 0)) { // 검색 조건과 키워드가 있다면
+
+            BooleanBuilder booleanBuilder = new BooleanBuilder(); // (
+
+            for (String type : types) {
+                switch (type) {
+                    case "k" :
+                        booleanBuilder.or(board.title.contains(keyword)); // title like ...
+                        break;
+                    case "d" :
+                        booleanBuilder.or(board.difficulty.in(difficulties));
+                        break;
+                    case "t":
+                        booleanBuilder.or(board.tagList.contains(tag));
+                }
+            } // end for
+
+            query.where(booleanBuilder); // )
+        } // end if
+
+        // bno > 0
+        query.where(board.id.gt(0L)); // and id > 0
+
+        query.where(board.email.eq(email)); // and email = :email
+
+        long count = query.fetchCount();
+
+        return count;
+    }
 }
