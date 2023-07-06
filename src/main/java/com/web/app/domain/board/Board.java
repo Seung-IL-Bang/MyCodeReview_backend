edd752660bb6263542740bc5f11a19161a2c694c
Bang <seungilbang@khu.ac.kr>
2023-07-06 17:42:40 +0900

    [FIX] About Board: DTO 이름 변경에 의한 매개변수 수정

--- src/main/java/com/web/app/domain/board/Board.java
+++ src/main/java/com/web/app/domain/board/Board.java
@@ -1,11 +1,9 @@
 package com.web.app.domain.board;
 
 import com.web.app.domain.BaseTimeEntity;
-import com.web.app.domain.review.Review;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -40,10 +38,6 @@ public class Board extends BaseTimeEntity {
 
     @ElementCollection
     private Set<String> tagList;
-
-    @OneToMany(mappedBy = "board")
-    private List<Review> reviewList;
-
 
     public void change(String title, String content, Set<String> tagList, String link, String difficulty) {
         this.title = title;
