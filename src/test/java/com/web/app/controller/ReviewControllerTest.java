48d5615a9cf2492bb89378aa4adbeefaec44a4e5
Seungil Linux <seungilbang@khu.ac.kr>
2023-07-31 19:15:15 +0900

    [TEST] ReviewControllerTest fixing on BDD

--- src/test/java/com/web/app/controller/ReviewControllerTest.java
+++ src/test/java/com/web/app/controller/ReviewControllerTest.java
@@ -9,11 +9,14 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -23,8 +26,6 @@ class ReviewControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private ObjectMapper objectMapper;
     @MockBean
     private ReviewService reviewService;
 
@@ -32,27 +33,28 @@ class ReviewControllerTest {
     private ModelMapper modelMapper;
 
 
-    // TODO : 인가를 위한 Authorization 헤더 JWT 토큰 추가
-//    @DisplayName("Review ID 로 해당 Review 를 조회한다.")
-//    @Test
-//    void getReview() throws Exception {
-//        //given
-//        Long id = 1L;
-//        ReviewResponseDTO reviewResponseDTO = ReviewResponseDTO.builder()
-//                .id(id)
-//                .subTitle("subtitle")
-//                .content("content")
-//                .build();
-//
-//        when(reviewService.read(id)).thenReturn(reviewResponseDTO);
-//
-//        // when // then
-//        mockMvc.perform(MockMvcRequestBuilders.get("/auth/board/review/1")
-//                        .header("Authorization"))
-//                .andDo(print())
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.subtitle").value("subtitle"))
-//                .andExpect(jsonPath("$.content").value("content"))
-//                .andExpect(jsonPath("$.id").value(1L));
-//    }
+
+    @DisplayName("Review ID 로 해당 Review 를 조회 한다.")
+    @Test
+    @WithMockUser
+    void getReview() throws Exception {
+        //given
+        Long id = 1L;
+        ReviewResponseDTO reviewResponseDTO = ReviewResponseDTO.builder()
+                .id(id)
+                .subTitle("subtitle")
+                .content("content")
+                .build();
+
+        given(reviewService.read(id)).willReturn(reviewResponseDTO);
+
+        // when // then
+        mockMvc.perform(MockMvcRequestBuilders.get(String.format("/auth/board/review/%d", id))
+                        .with(csrf()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.subTitle").value("subtitle"))
+                .andExpect(jsonPath("$.content").value("content"))
+                .andExpect(jsonPath("$.id").value(1L));
+    }
 }
