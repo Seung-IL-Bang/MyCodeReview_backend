6ae334a0c34d13b7c43a9fbd6ccc439724c057f0
Seungil Linux <seungilbang@khu.ac.kr>
2023-07-31 22:30:12 +0900

    [TEST] Add ReviewFixtureFactory

---
+++ src/test/java/com/web/app/fixture/ReviewFixtureFactory.java
@@ -0,0 +1,44 @@
+package com.web.app.fixture;
+
+import com.web.app.domain.review.Review;
+import com.web.app.dto.ReviewRequestDTO;
+import com.web.app.dto.ReviewResponseDTO;
+import org.jeasy.random.EasyRandom;
+import org.jeasy.random.EasyRandomParameters;
+
+public class ReviewFixtureFactory {
+
+    public static Review create() {
+        EasyRandomParameters param = new EasyRandomParameters();
+        param.stringLengthRange(3, 10);
+        return new EasyRandom(param).nextObject(Review.class);
+    }
+
+    public static ReviewRequestDTO createRequestDTO() {
+        EasyRandomParameters param = new EasyRandomParameters();
+        param.stringLengthRange(3, 10);
+        return new EasyRandom(param).nextObject(ReviewRequestDTO.class);
+    }
+
+    public static ReviewRequestDTO createRequestDTO(Long seed) {
+        EasyRandomParameters param = new EasyRandomParameters().seed(seed);
+        param.stringLengthRange(3, 10);
+        return new EasyRandom(param).nextObject(ReviewRequestDTO.class);
+    }
+
+    public static ReviewResponseDTO createResponseDTO() {
+        EasyRandomParameters param = new EasyRandomParameters();
+        param.stringLengthRange(3, 10);
+        return new EasyRandom(param).nextObject(ReviewResponseDTO.class);
+    }
+
+    public static ReviewResponseDTO createResponseDTO(Long seed) {
+        EasyRandomParameters param = new EasyRandomParameters().seed(seed);
+        param.stringLengthRange(3, 10);
+        return new EasyRandom(param).nextObject(ReviewResponseDTO.class);
+    }
+
+
+
+
+}
