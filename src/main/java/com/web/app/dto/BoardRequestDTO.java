d7cc18abc819f2ba1817b406a653651bffe24f4a
Seungil Linux <seungilbang@khu.ac.kr>
2023-08-02 01:57:53 +0900

    [UPD] requestDTO: @NotBlank 추가

--- src/main/java/com/web/app/dto/BoardRequestDTO.java
+++ src/main/java/com/web/app/dto/BoardRequestDTO.java
@@ -1,5 +1,8 @@
 package com.web.app.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.util.Set;
@@ -11,11 +14,22 @@ import java.util.Set;
 @NoArgsConstructor
 public class BoardRequestDTO {
 
+    @NotBlank(message = "제목은 필수입니다.")
     private String title;
+
+    @NotBlank(message = "본문에 글을 작성해주세요.")
     private String content;
+
     private Set<String> tagList;
+
     private String link;
+
+    @NotBlank(message = "난이도를 선택해주세요.")
     private String difficulty;
+
+    @NotBlank(message = "작성자를 알 수 없습니다.")
     private String writer;
+
+    @NotBlank(message = "작성자의 이메일을 확인할 수 없습니다.")
     private String email;
 }
