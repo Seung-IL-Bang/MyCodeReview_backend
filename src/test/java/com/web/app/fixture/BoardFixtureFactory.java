f8f4da70fdb0062f8633d8f370560c9aeb4885bb
Seungil <seungilbang@khu.ac.kr>
2023-08-03 10:44:53 +0900

    [TEST] BoardFixtureFactory: excludeField 추가

--- src/test/java/com/web/app/fixture/BoardFixtureFactory.java
+++ src/test/java/com/web/app/fixture/BoardFixtureFactory.java
@@ -5,18 +5,21 @@ import com.web.app.dto.BoardRequestDTO;
 import com.web.app.dto.BoardResponseDTO;
 import org.jeasy.random.EasyRandom;
 import org.jeasy.random.EasyRandomParameters;
+import org.jeasy.random.FieldPredicates;
 
 public class BoardFixtureFactory {
 
     public static Board create() {
         EasyRandomParameters param = new EasyRandomParameters();
         param.stringLengthRange(3, 10);
+        param.excludeField(FieldPredicates.named("id"));
         return new EasyRandom(param).nextObject(Board.class);
     }
 
     public static Board create(Long seed) {
         EasyRandomParameters param = new EasyRandomParameters().seed(seed);
         param.stringLengthRange(3, 10);
+        param.excludeField(FieldPredicates.named("id"));
         return new EasyRandom(param).nextObject(Board.class);
     }
 
