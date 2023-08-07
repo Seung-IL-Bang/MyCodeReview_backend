15b3c8a394435bdde12caa8a23109f2bd92eec63
Seungil Linux <seungilbang@khu.ac.kr>
2023-08-07 20:20:39 +0900

    [FIX] LikesRepository: nativeQuery 문법 오류 수정

--- src/main/java/com/web/app/repository/LikesRepository.java
+++ src/main/java/com/web/app/repository/LikesRepository.java
@@ -8,6 +8,6 @@ import java.util.List;
 
 public interface LikesRepository extends JpaRepository<Likes, Long> {
 
-    @Query(value = "select l.id from Likes l where l.board_id = :boardId and l.member_email = :memberEmail", nativeQuery = true)
+    @Query(value = "select l.id from likes l where l.board_id = :boardId and l.member_email = :memberEmail", nativeQuery = true)
     List<Long> isLiked(Long boardId, String memberEmail);
 }
