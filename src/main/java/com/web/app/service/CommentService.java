4a203c7ef59025a7c323ac6f9973fd056c6fc68a
Seungil <seungilbang@khu.ac.kr>
2023-08-10 16:44:48 +0900

    [Feat] Comment 댓글 API 완료

--- src/main/java/com/web/app/service/CommentService.java
+++ src/main/java/com/web/app/service/CommentService.java
@@ -1,8 +1,14 @@
 package com.web.app.service;
 
 import com.web.app.dto.CommentRequestDTO;
+import com.web.app.exception.BusinessLogicException;
+import jakarta.servlet.http.HttpServletRequest;
 
 public interface CommentService {
 
     void register(CommentRequestDTO commentRequestDTO);
+
+    void update(CommentRequestDTO commentRequestDTO, HttpServletRequest request) throws BusinessLogicException;
+
+    void remove(Long id, HttpServletRequest request) throws BusinessLogicException;
 }
