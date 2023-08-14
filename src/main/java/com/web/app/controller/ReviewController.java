efd3b840303291884efc800af62268a1319487b4
Seungil Linux <seungilbang@khu.ac.kr>
2023-08-14 21:57:19 +0900

    [UPD] Review CRUD 사용자 권한 검증 추가

--- src/main/java/com/web/app/controller/ReviewController.java
+++ src/main/java/com/web/app/controller/ReviewController.java
@@ -4,6 +4,7 @@ import com.web.app.domain.review.Review;
 import com.web.app.dto.ReviewRequestDTO;
 import com.web.app.dto.ReviewResponseDTO;
 import com.web.app.service.ReviewService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
@@ -20,32 +21,32 @@ public class ReviewController {
     private final ModelMapper modelMapper;
     
     @GetMapping("/board/review/{id}")
-    public ResponseEntity getReview(@PathVariable("id") @Positive Long id) {
+    public ResponseEntity getReview(@PathVariable("id") @Positive Long id, HttpServletRequest request) {
 
-        ReviewResponseDTO response = reviewService.read(id);
+        ReviewResponseDTO response = reviewService.read(id, request);
 
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @PostMapping("/auth/board/review/{board-id}")
-    public ResponseEntity postReview(@Valid @RequestBody ReviewRequestDTO reviewRequestDTO, @PathVariable("board-id") Long boardId) {
+    public ResponseEntity postReview(HttpServletRequest request, @Valid @RequestBody ReviewRequestDTO reviewRequestDTO, @PathVariable("board-id") Long boardId) {
 
-        Long registeredId = reviewService.register(modelMapper.map(reviewRequestDTO, Review.class), boardId);
+        Long registeredId = reviewService.register(request, modelMapper.map(reviewRequestDTO, Review.class), boardId);
         return new ResponseEntity(registeredId, HttpStatus.CREATED);
     }
 
     @PutMapping("/auth/board/review/{id}")
-    public ResponseEntity putReview(@Valid @RequestBody ReviewRequestDTO reviewRequestDTO, @PathVariable("id") Long id) {
+    public ResponseEntity putReview(HttpServletRequest request, @Valid @RequestBody ReviewRequestDTO reviewRequestDTO, @PathVariable("id") Long id) {
 
-        reviewService.modify(reviewRequestDTO, id);
+        reviewService.modify(request, reviewRequestDTO, id);
 
         return new ResponseEntity("Updated Id: " + id + " Review", HttpStatus.OK);
     }
 
     @DeleteMapping("/auth/board/review/{id}")
-    public ResponseEntity deleteReview(@PathVariable("id") Long id) {
+    public ResponseEntity deleteReview(HttpServletRequest request, @PathVariable("id") Long id) {
 
-        reviewService.remove(id);
+        reviewService.remove(request, id);
 
         return new ResponseEntity("Deleted " + id + " Review", HttpStatus.OK);
     }
