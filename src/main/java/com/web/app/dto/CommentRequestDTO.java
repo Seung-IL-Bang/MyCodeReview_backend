6b4d072d98838a8092e94c0560b980a7fc1b18c3
Seungil Linux <seungilbang@khu.ac.kr>
2023-08-14 17:09:52 +0900

    [FIX] Comment 요청 & 응답 dto 수정

--- src/main/java/com/web/app/dto/CommentRequestDTO.java
+++ src/main/java/com/web/app/dto/CommentRequestDTO.java
@@ -7,11 +7,13 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class CommentRequestDTO {
 
     private Long id;
--- src/main/java/com/web/app/dto/CommentResponseDTO.java
+++ src/main/java/com/web/app/dto/CommentResponseDTO.java
@@ -1,9 +1,9 @@
 package com.web.app.dto;
 
+import com.web.app.domain.comment.Comment;
 import lombok.*;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Getter
 @Setter
@@ -15,6 +15,15 @@ public class CommentResponseDTO {
     private Long id;
     private String content;
     private String memberEmail;
+    private String memberName;
     private LocalDateTime modifiedAt;
+
+    public CommentResponseDTO(Comment comment) {
+        this.id = comment.getId();
+        this.content = comment.getContent();
+        this.memberName = comment.getMember().getName();
+        this.memberEmail = comment.getMember().getEmail();
+        this.modifiedAt = comment.getModifiedAt();
+    }
 
 }
