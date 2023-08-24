6698cf6d6675fbdc1334c1943289755d14a10f4e
Seungil Linux <seungilbang@khu.ac.kr>
2023-08-24 23:09:18 +0900

    [FIX] CommentRepository: 조회 정렬 방법 변경 Desc -> Asc

--- src/main/java/com/web/app/repository/CommentRepository.java
+++ src/main/java/com/web/app/repository/CommentRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
-    List<Comment> findAllByBoardIsOrderByCreatedAtDesc(Board board);
+    List<Comment> findAllByBoardIsOrderByCreatedAtAsc(Board board);
 }
