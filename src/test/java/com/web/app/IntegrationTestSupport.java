77415d589ff32855d53124714e24777a8cfa6d4c
Bang <seungilbang@khu.ac.kr>
2023-09-26 18:54:44 +0900

    [ADD] Service, Repository 계층 테스트 통합 환경 클래스 추가

---
+++ src/test/java/com/web/app/IntegrationTestSupport.java
@@ -0,0 +1,13 @@
+package com.web.app;
+
+
+import com.web.app.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+public abstract class IntegrationTestSupport {
+
+}
