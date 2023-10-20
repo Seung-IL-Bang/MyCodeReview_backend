135700e2879c4e6089e3d31913b0f2d19eb15d87
Bang <seungilbang@khu.ac.kr>
2023-10-20 17:46:02 +0900

    [FIX] 쿼리 성능 개선을 위한 인덱스 생성 파일 수정

--- src/main/resources/index.sql
+++ src/main/resources/index.sql
@@ -1,16 +1,5 @@
-/*
- * My Review 조회 인덱스: GET /auth/board/v2/list
- * explain select b from Board b where b.email = :email
- */
 CREATE INDEX email_idx ON Board (email);
 
+CREATE INDEX created_at_idx ON Board (created_at);
 
-/*
- * public search 조회 인덱스: GET /board/list?types=k&keyword=???
- * explain
-    select board
-    from Board board
-    where board.title like ?1 escape '!' and board.id > ?2
-    order by board.createdAt desc
- */
-CREATE INDEX title_idx ON Board (title);
+CREATE INDEX difficulty_idx on Board(difficulty);
