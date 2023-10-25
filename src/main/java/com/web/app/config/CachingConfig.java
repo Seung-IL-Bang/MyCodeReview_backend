16850e10528f26a71febdb7e9e8d76c1eec41abe
Bang <seungilbang@khu.ac.kr>
2023-10-25 22:40:46 +0900

    [UPD] Redis Connection Pooling 테스트

--- src/main/java/com/web/app/config/CachingConfig.java
+++ src/main/java/com/web/app/config/CachingConfig.java
@@ -1,6 +1,8 @@
 package com.web.app.config;
 
 
+import io.lettuce.core.resource.ClientResources;
+import org.apache.commons.pool2.impl.GenericObjectPoolConfig;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,9 +10,14 @@ import org.springframework.data.redis.cache.CacheKeyPrefix;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettucePoolingClientConfiguration;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import redis.clients.jedis.JedisPoolConfig;
 
 import java.time.Duration;
 import java.util.HashMap;
@@ -47,5 +54,39 @@ public class CachingConfig {
                 .cacheDefaults(defaultConfiguration)
                 .withInitialCacheConfigurations(configMap)
                 .build();
+    }
+
+
+//    @Bean
+//    public JedisConnectionFactory jedisConnectionFactory() {
+//
+//        JedisPoolConfig poolConfig = new JedisPoolConfig();
+//        poolConfig.setMaxTotal(10);
+//        poolConfig.setMaxIdle(7);
+//        poolConfig.setMinIdle(3);
+//
+//        JedisConnectionFactory factory = new JedisConnectionFactory();
+//        factory.setPoolConfig(poolConfig);
+//
+//        return factory;
+//    }
+
+    @Bean
+    public LettucePoolingClientConfiguration lettucePoolConfig(ClientResources clientResources) {
+        return LettucePoolingClientConfiguration.builder()
+                .poolConfig(new GenericObjectPoolConfig())
+                .clientResources(clientResources)
+                .build();
+    }
+
+    @Bean
+    public RedisStandaloneConfiguration redisStandaloneConfiguration() {
+        return new RedisStandaloneConfiguration("localhost", 6379);
+    }
+
+    @Bean
+    public LettuceConnectionFactory lettuceConnectionFactory(RedisStandaloneConfiguration redisStandaloneConfiguration,
+                                                             LettucePoolingClientConfiguration lettucePoolConfig) {
+        return new LettuceConnectionFactory(redisStandaloneConfiguration, lettucePoolConfig);
     }
 }
