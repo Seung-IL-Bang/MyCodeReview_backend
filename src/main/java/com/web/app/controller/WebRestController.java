6901bfa1ee926bb5bbd5fad8480e73ca0be017f2
Bang <seungilbang@khu.ac.kr>
2023-11-02 15:57:23 +0900

    [UPD] 트래픽 분산 처리 확인을 위한 API 추가

--- src/main/java/com/web/app/controller/WebRestController.java
+++ src/main/java/com/web/app/controller/WebRestController.java
@@ -6,6 +6,9 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @RestController
 @AllArgsConstructor
 public class WebRestController {
@@ -14,7 +17,14 @@ public class WebRestController {
 
 
     @GetMapping("/port")
-    public String getProfile() {
+    public String getPORT() {
         return env.getProperty("local.server.port");
+    }
+
+    @GetMapping("/ip")
+    public String getIP() throws UnknownHostException {
+        InetAddress inetAddress = InetAddress.getLocalHost();
+        String ip = inetAddress.getHostAddress();
+        return ip;
     }
 }
