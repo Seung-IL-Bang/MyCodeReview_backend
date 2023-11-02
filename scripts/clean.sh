83d4f8f482512cee401f997586ab9e8c04c40465
Bang <seungilbang@khu.ac.kr>
2023-11-02 17:16:03 +0900

    [FIX] CI/CD 정상 작동 확인을 위한 수정 사항

---
+++ scripts/clean.sh
@@ -0,0 +1,9 @@
+echo ">>> Start BeforeInstall"
+
+
+sudo rm -rf /home/ubuntu/appspec.yml
+sudo rm -rf /home/ubuntu/docker-compose.yml
+sudo rm -rf /home/ubuntu/deploy.sh
+
+
+echo ">>> End BeforeInstall"
